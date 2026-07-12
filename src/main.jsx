576433acd2206ed5a9a523fed0568711d7138466
gilson gi <gilsongillazaro@gmail.com>
2026-07-12 15:13:10 +0100

    integracao com o vercel analitics

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { CarrinhoProvider } from "./CarrinhoContext.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <CarrinhoProvider>
     <App />
+    <Analytics />
   </CarrinhoProvider>,
 );
